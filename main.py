c9df3525813542fa30c00118afe1cc5f4202be07
Marten0612 <marten1201@hotmail.com>
2022-05-02 22:08:33 +0200

    startup

---
+++ main.py
@@ -0,0 +1,25 @@
+from turtle import speed
+from matplotlib.pyplot import pink
+
+
+class sorter:
+    def __init__(self) -> None:
+        pass
+
+class camera:
+    def __init__(self) -> None:
+        pass
+
+    def connect(self):
+        pass
+
+
+
+class hopper:
+    def __init__(self, hopper_pin, hopper_speed):
+        self.hopper_pin = hopper_pin
+        self.hopper_speed = hopper_speed
+        pass
+
+    def turn_on(self):
+        pass
