375ddd90468d52dfde01fba08586581a95d2425d
Jipr <64846573+Jipr@users.noreply.github.com>
2022-05-17 14:44:43 +0200

    d
    d

--- ML test.py
+++ ML test.py
@@ -2,12 +2,12 @@ import torch
 
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
-
+model = torch.hub.load('../yolov5', 'custom', path="Models/labels.pt", source='local')  # local repo
 # Images
 img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
 
 # Inference
 results = model(img)
-
+results.pandas().xyxy[0].to_json(orient="records")
 # Results
-results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+results.print()  # or .show(), .save(), .crop(), .pandas(), etc.cl
