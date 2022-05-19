4ba414cf995b8ed83b91977d6877b4dd466be1a3
Jipr <64846573+Jipr@users.noreply.github.com>
2022-05-19 10:31:35 +0200

    d

--- Sorter Arduino/src/main.cpp
+++ Sorter Arduino/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
+int x;
+
 void setup() {
-  // put your setup code here, to run once:
+Serial.begin(115200);
+Serial.setTimeout(1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+while (!Serial.available());
+x = Serial.readString().toInt();
+Serial.print(x + 1);
 }
