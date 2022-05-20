289aa3b05757a916236eb33c9684945499a43ea0
Jipr <64846573+Jipr@users.noreply.github.com>
2022-05-20 15:57:02 +0200

    s

--- Serial test.py
+++ Serial test.py
@@ -8,33 +8,35 @@ cam2 = cv2.VideoCapture(2)
 cam3 = cv2.VideoCapture(3)
 
 arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+counter = 0
 
 while True:
     time.sleep(0.05)
     data = arduino.readline()
     data = int.from_bytes(data,"big")#.decode('ascii')
-    counter = 0
+    
     if (data == 49):
         print("HET LUKT")
-        time.sleep(1)
+        time.sleep(3)
         ret1, frame1 = cam1.read()
         ret2, frame2 = cam2.read()
         ret3, frame3 = cam3.read()
         if not (ret1 or ret2 or ret3):
             print("failed to grab frame")
             break
-        cv2.imshow("t", frame1)
-        cv2.imshow("d", frame2)
-        cv2.imshow("s", frame3)
+        # cv2.imshow("t", frame1)
+        # cv2.imshow("d", frame2)
+        # cv2.imshow("s", frame3)
         number1 = counter 
-        number2 = counter + 1
-        number3 = counter + 2
-        cv2.imwrite("C:Users\jipra\Documents\Training_data\\Brick%d.jpg" % number1, frame1)
-        cv2.imwrite("C:Users\jipra\Documents\Training_data\\Brick%d.jpg" % number2, frame2)
-        cv2.imwrite("C:Users\jipra\Documents\Training_data\\Brick%d.jpg" % number3, frame3)
-        counter += 1
-        cv2.waitKey(0) 
-        cv2.destroyAllWindows() 
+        number2 = (counter + 1)
+        number3 = (counter + 2)
+        #cv2.imwrite("Brick%d.jpg" % number1, frame1)
+        cv2.imwrite(r"C:\Users\jipra\Documents\Training_data\Brick%d.jpg" % number1, frame1)
+        cv2.imwrite(r"C:\Users\jipra\Documents\Training_data\Brick%d.jpg" % number2, frame2)
+        cv2.imwrite(r"C:\Users\jipra\Documents\Training_data\Brick%d.jpg" % number3, frame3)
+        counter += 3
+        # cv2.waitKey(0) 
+        # cv2.destroyAllWindows() 
 
 cam1.release()
 cam2.release()
